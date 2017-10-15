a90512da205df0f37f91e679baff716f293419b5
User <User@192.168.0.241>
2017-10-16 00:26:19 +0100

    Extracted JSON variables from API call and introduced code to print out basic station data

--- JavaBikes/src/readData/AccessUrl.java
+++ JavaBikes/src/readData/AccessUrl.java
@@ -1,40 +1,50 @@
 package readData;
 
 import java.net.*;
-import java.io.*;
-import java.util.HashMap;
-import java.util.Scanner;
-
+import java.util.Date;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONException; 
-import org.json.JSONObject; 
-import org.json.JSONArray; 
+import org.json.JSONTokener;
 
 public class AccessUrl {
     public static void main(String[] args) throws Exception {
 
     	String root = "https://api.jcdecaux.com/vls/v1/stations?contract="; //base url
         String city = "Dublin"; //enter city here
-        String apiKey = "&apiKey=30ae8e68ff6ff0175b969c62ee4bd43c4c32bb0f"; //secure key		
-    	URL api = new URL(root + city + apiKey);
-        
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(api.openStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine); 
-        in.close();
-        
-        String out = new Scanner(api.openStream(), "UTF-8").useDelimiter("\\A").next();
-        System.out.println("out" + out);
-        JSONObject station = new JSONObject(out.trim());
-        String stazione = (String) station.get("name");
-        System.out.println("stazione" + stazione);
-        JSONObject json = new JSONObject(api);
-        String title = (String) json.get("file");
-        System.out.println("file" + title);
-        JSONObject jsonObject = new JSONObject(api); 
-        System.out.println(jsonObject);
+        String apiKey = "&apiKey=30ae8e68ff6ff0175b969c62ee4bd43c4c32bb0f"; //secure key		   	
+    	URL urlBike = new URL(root + city + apiKey);
+    	JSONTokener tokenerz = new JSONTokener(urlBike.openStream());
+    	JSONArray objz = new JSONArray(tokenerz);
+    	
+    	//data variables
+    	int StationId;
+    	String stationName;
+    	String stationAddress;
+    	long stationLatitude;
+    	long stationLongitude;
+    	boolean stationBanking;
+    	boolean stationBonus;
+    	String stationStatus;
+    	String stationCity;
+    	int stationCapacity;
+    	int stationFreeBikes;
+    	int stationFreeStands;
+    	long lastUpdate;
+
+    	
+    	for (int i = 0; i < objz.length(); ++i) {
+    	    JSONObject bikeobj = objz.getJSONObject(i);
+    	    stationName = bikeobj.getString("name");
+    	    stationAddress = bikeobj.getString("address");
+    	    stationCapacity = bikeobj.getInt("bike_stands");
+    	    stationFreeBikes = bikeobj.getInt("available_bikes");
+    	    stationFreeStands = bikeobj.getInt("available_bike_stands");
+    	    lastUpdate = bikeobj.getLong("last_update");
+        	Date lastDate = new Date(lastUpdate);
+    	    System.out.println("The station is " +  stationAddress + ". It has a capacity of " + stationCapacity + " bikes. There are " + stationFreeBikes + " available bikes and " + stationFreeStands + " free bike stands as of " + lastDate);
+    	    // ...
+    	}
+    	
 
     }
 }
