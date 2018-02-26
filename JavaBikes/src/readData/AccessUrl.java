3b29bc4b152e51fe0b81710da30e06e9f3c6d6a9
User <User@192.168.0.241>
2018-02-26 23:35:03 +0000

    Successful SQL statement to insert into local database and changing variables

--- JavaBikes/src/readData/AccessUrl.java
+++ JavaBikes/src/readData/AccessUrl.java
@@ -10,7 +10,9 @@ import org.json.JSONTokener;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AccessUrl {
     public static void main(String[] args) throws Exception {
@@ -32,13 +34,16 @@ public class AccessUrl {
     	boolean stationBonus;
     	String stationStatus;
     	String stationCity;
+    	String contractName;
     	int stationCapacity;
     	int stationFreeBikes;
     	int stationFreeStands;
     	long lastUpdate;
-
-    	connect();
     	
+
+    	String url = "jdbc:sqlite:C://sqlite/db/tests.db";
+    	Connection c=(Connection) DriverManager.getConnection(url);	
+    	Statement s=c.createStatement();
     	for (int i = 0; i < objz.length(); ++i) {
     	    JSONObject stationObjects = objz.getJSONObject(i);
     	    JSONObject locationObjects = objz.getJSONObject(i).getJSONObject("position");
@@ -47,17 +52,31 @@ public class AccessUrl {
     	    stationAddress = stationObjects.getString("address");
     	    stationLatitude = locationObjects.getDouble("lat");
     	    stationLongitude = locationObjects.getDouble("lng");
-    	    stationBanking = stationObjects.getBoolean("bonus");
+    	    stationBanking = stationObjects.getBoolean("banking");
+    	    stationBonus = stationObjects.getBoolean("bonus");
+    	    contractName = stationObjects.getString("contract_name");
     	    stationStatus = stationObjects.getString("status");
     	    stationCapacity = stationObjects.getInt("bike_stands");
     	    stationFreeStands = stationObjects.getInt("available_bike_stands");
     	    stationFreeBikes = stationObjects.getInt("available_bikes");
     	    lastUpdate = stationObjects.getLong("last_update");
-    	    //System.out.println("The station is " +  stationAddress + ", located at (" + stationLatitude + ", " + stationLongitude + "). It has a capacity of " + stationCapacity + " bikes. There are " + stationFreeBikes + " available bikes and " + stationFreeStands + " free bike stands as of " + lastDate);
-    	    // ...
-        	//System.out.println("[" + stationLongitude + "," + stationLatitude + "]" );
-    	    String sql = "INSERT INTO dublinbikes(address,available_bike_stands,available_bikes,"
-    	    		+ "banking,bike_stands,bonus,contract_name,last_update,name,number,position,status,update_time,update_date) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    	    String sql = "INSERT INTO dublinbikes VALUES  (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    		PreparedStatement pstmt = c.prepareStatement(sql);
+    		pstmt.setString(1, stationAddress );
+    		pstmt.setInt(2, stationFreeStands);
+    		pstmt.setInt(3, stationFreeBikes);
+    		pstmt.setBoolean(4, stationBanking);
+    		pstmt.setInt(5,stationCapacity);
+       		pstmt.setBoolean(6,stationBonus);
+       		pstmt.setString(7,contractName);
+       		pstmt.setLong(8,lastUpdate);
+       		pstmt.setString(9,stationName);
+       		pstmt.setInt(10,StationId);
+       		pstmt.setDouble(11,stationLatitude);
+       		pstmt.setString(12,stationStatus);
+       		pstmt.setLong(13,lastUpdate);
+       		pstmt.setLong(14,lastUpdate);
+    		pstmt.executeUpdate();
     	}
     	    	
        
@@ -69,10 +88,13 @@ public class AccessUrl {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
+            System.out.println("\nConnecting to database...");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("SUCCESS!\n");
         return conn;
+        
     }
     
     
